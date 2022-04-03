b7f6e167549f8ce0056983e90ea1bb9f168dd2df
조인혁 <alzpqm@naver.com>
2022-04-04 05:09:43 +0900

    feat: 회원가입, 로그인 구현

---
+++ src/main/java/com/fireprohibition/CBomb/authentication/UserDetailServiceImpl.java
@@ -0,0 +1,22 @@
+package com.fireprohibition.CBomb.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.fireprohibition.CBomb.domain.user.UserRepository;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return new CustomUserDetails(userRepository.findByUsername(username));
+	}
+}
--- src/main/java/com/fireprohibition/CBomb/controller/UserController.java
+++ src/main/java/com/fireprohibition/CBomb/controller/UserController.java
@@ -1,4 +1,54 @@
 package com.fireprohibition.CBomb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fireprohibition.CBomb.dto.RegisterForm;
+import com.fireprohibition.CBomb.domain.user.UserRepository;
+import com.fireprohibition.CBomb.service.RegisterService;
+
+@Controller
 public class UserController {
+
+	@Autowired
+	private RegisterService registerService;
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/login")
+	public String loginPage() {
+		return "/loginForm";
+	}
+
+	@GetMapping("/register")
+	public String registerPage() {
+		return "/register";
+	}
+
+	@PostMapping("/register")
+	public String register(@ModelAttribute RegisterForm form,
+			BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "/register";
+		} else if(userRepository.findByUsername(form.getUsername()).isPresent()){
+			bindingResult.reject("registerFail", "이미 존재하는 아이디입니다.");
+			return "/register";
+		} else if (!form.getPassword().equals(form.getPasswordCheck())) {
+			bindingResult.reject("registerFail", "비밀번호가 일치하지 않습니다.");
+			return "/register";
+		}
+		registerService.joinUser(form.getUsername(), form.getPassword());
+		return "redirect:/";
+	}
+
+	@GetMapping("/test")
+	public @ResponseBody
+	String test() {
+		return "접근가능";
+	}
 }
