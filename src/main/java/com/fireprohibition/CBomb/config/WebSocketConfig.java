7f5e64cb3d73236d67325c17053548c29c0a061e
TaeheeGu <71616468+TaeheeGu@users.noreply.github.com>
2022-04-28 22:39:45 +0900

    feat:stomp chat

--- src/main/java/com/fireprohibition/CBomb/config/WebSocketConfig.java
+++ src/main/java/com/fireprohibition/CBomb/config/WebSocketConfig.java
@@ -20,6 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/stomp/chat").setAllowedOriginPatterns("*").withSockJS();
     }
 }
